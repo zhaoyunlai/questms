45bc3079e413bc0a8fd49e43419dc17b49d3564d
Konjacor <weichuanru@126.com>
2022-09-05 15:02:05 +0800

    编写qvs_survey表的crud

--- service/service_qvs/src/main/java/com/sdnu/iosclub/qvs/service/QvsSurveyService.java
+++ service/service_qvs/src/main/java/com/sdnu/iosclub/qvs/service/QvsSurveyService.java
@@ -1,7 +1,13 @@
 package com.sdnu.iosclub.qvs.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sdnu.iosclub.qvs.entity.QvsSurvey;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.sdnu.iosclub.serviceutil.R;
+
+import java.sql.Wrapper;
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +18,38 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-09-05
  */
 public interface QvsSurveyService extends IService<QvsSurvey> {
+    /**
+     * 获取所有问卷信息
+     * @return 所有问卷信息
+     */
+    List<QvsSurvey> getAllSurvey();
 
+    /**
+     * 分页获取问卷信息
+     * @param current 当前页数
+     * @param size 每页的数量
+     * @return 分页数据
+     */
+    Page<QvsSurvey> pageGetSurvey(long current,long size);
+
+    /**
+     * 添加问卷
+     * @param qvsSurvey 问卷实体
+     * @return 是否添加成功
+     */
+    boolean addSurvey(QvsSurvey qvsSurvey);
+
+    /**
+     * 修改问卷
+     * @param qvsSurvey 修改后的问卷实体
+     * @return 是否修改成功
+     */
+    boolean updateSurvey(QvsSurvey qvsSurvey);
+
+    /**
+     * 根据id删除问卷
+     * @param id 问卷id
+     * @return 是否删除成功
+     */
+    boolean deleteSurvey(String id);
 }
--- service/service_qvs/src/main/java/com/sdnu/iosclub/qvs/service/impl/QvsSurveyServiceImpl.java
+++ service/service_qvs/src/main/java/com/sdnu/iosclub/qvs/service/impl/QvsSurveyServiceImpl.java
@@ -1,10 +1,16 @@
 package com.sdnu.iosclub.qvs.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sdnu.iosclub.qvs.entity.QvsSurvey;
 import com.sdnu.iosclub.qvs.mapper.QvsSurveyMapper;
 import com.sdnu.iosclub.qvs.service.QvsSurveyService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sdnu.iosclub.serviceutil.R;
 import org.springframework.stereotype.Service;
+
+import java.sql.Wrapper;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +23,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class QvsSurveyServiceImpl extends ServiceImpl<QvsSurveyMapper, QvsSurvey> implements QvsSurveyService {
 
+    @Override
+    public List<QvsSurvey> getAllSurvey() {
+        QueryWrapper<QvsSurvey> wrapper = new QueryWrapper<>();
+        wrapper.orderByDesc("update_time");
+        return this.list(wrapper);
+    }
+
+    @Override
+    public Page<QvsSurvey> pageGetSurvey(long current, long size) {
+        Page<QvsSurvey> page = new Page<>(current,size);
+        QueryWrapper<QvsSurvey> wrapper = new QueryWrapper<>();
+        wrapper.orderByDesc("update_time");
+        this.page(page,wrapper);
+        return page;
+    }
+
+
+    @Override
+    public boolean addSurvey(QvsSurvey qvsSurvey) {
+        return this.save(qvsSurvey);
+    }
+
+    @Override
+    public boolean updateSurvey(QvsSurvey qvsSurvey) {
+        return this.updateById(qvsSurvey);
+    }
+
+    @Override
+    public boolean deleteSurvey(String id) {
+        QueryWrapper<QvsSurvey> wrapper = new QueryWrapper<>();
+        wrapper.eq("id",id);
+        return this.remove(wrapper);
+    }
 }
